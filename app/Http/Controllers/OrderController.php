2eda5ade6fdd81a3072bb49cb5178b0706b19d55
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2023-03-15 23:05:52 +0700

    DEPLOY

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -64,7 +64,7 @@ class OrderController extends Controller
     public function callback(Request $request)
     {
         $serverKey = config('midtrans.server_key');
-        $hashed = hash("sha12",$request->order_id.$request->status_code.$request->gross_amount.$serverKey);
+        $hashed = hash("sha512",$request->order_id.$request->status_code.$request->gross_amount.$serverKey);
         if($hashed == $request->signature_key){
             if($request->transaction_status == 'capture'){
                 $order == Order::where('id',$request->order_id);
