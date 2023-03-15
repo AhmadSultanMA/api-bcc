f31182c9172aae60791283d77d43f6a98b86a081
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2023-03-15 22:39:18 +0700

    DEPLOY

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -66,7 +66,7 @@ class OrderController extends Controller
         $serverKey = config('midtrans.server_key');
         $hashed = Hash::make($request->order_id.$request->status_code.$request->gross_amount.$serverKey);
         if($hashed == $request->signature_key){
-            if($request->transaction_status == 'settlement'){
+            if($request->transaction_status == 'capture'){
                 $order == Order::where('id',$request->order_id);
                 $order->status = 'Paid';
                 $order->save();
