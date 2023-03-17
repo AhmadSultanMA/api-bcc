84368612ebbf531ca0f69c517cfe20e9bc10ccb3
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2023-03-17 08:06:42 +0700

    DEPLOY

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -69,7 +69,7 @@ class OrderController extends Controller
         $hashed = hash("sha512",$request->order_id.$request->status_code.$request->gross_amount.$serverKey);
         if($hashed == $request->signature_key){
             if($request->transaction_status == 'capture'){
-                $order == Order::where('id',$request->order_id);
+                $order = Order::where('id',$request->order_id);
                 $order->status = 'Paid';
                 $order->save();
 
