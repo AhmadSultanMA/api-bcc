485bbee42443ed2e075573c44c1c666828e36f2d
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2023-03-17 09:55:09 +0700

    DEPLOY

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -65,7 +65,6 @@ class OrderController extends Controller
                 'authentication'=> true,
             ),
             'customer_details' => array(
-                'first_name' => $data->user->name,
                 'email' => $data->user->email,
                 'phone' => $data->user->nomor,
             ),
